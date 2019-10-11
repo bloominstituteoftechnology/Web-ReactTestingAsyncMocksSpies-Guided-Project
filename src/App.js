249a01f61577dfe80cd54dace25559e0a83d9da2
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-11 14:19:06 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setTimeout(() => {
+      setCount(count + 1)
+    },100)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-log" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload THE CHANGE!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>the count is {count}</div>
+        <button onClick={increaseCount}>increase</button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import * as rtl from '@testing-library/react';
 import App from './App';
 
@@ -12,5 +12,16 @@ beforeEach(() => {
 });
 
 it('matches snapshot', () => {
-  expect(wrapper.container).toMatchSnapshot()
+  expect(wrapper.container).toMatchSnapshot();
+});
+
+it('can increase from 0 to 1 by clicking button', async () => {
+  const button = wrapper.queryByText(/increase/);
+
+  expect(wrapper.queryByText(/the count is 0/)).toBeInTheDocument()
+  expect(wrapper.queryByText(/the count is 1/)).not.toBeInTheDocument();
+
+  rtl.fireEvent.click(button)
+
+  expect(await wrapper.findByText(/the count is 1/)).toBeInTheDocument();
 });

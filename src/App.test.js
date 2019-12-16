872a06df982c1eafe61fd549559703e0ff9c3810
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-16 10:29:34 +0100

    tip

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// STEP 1 pull deps in
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+
+// react testing library requires a "cleanup" operation
+// after each test
+// STEP 2 set up cleaning up in afterEach
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
